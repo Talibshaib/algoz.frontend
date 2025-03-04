6263498721d5a17354305cf7cbfd0f0d37a96d89
talib <mdtalib23038@gmail.com>
2025-03-04 15:49:54 +0530

    chore: type bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <Providers attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <Providers>
           {children}
         </Providers>
       </body> 
